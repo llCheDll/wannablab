4c7e2301922f36972df2bdf87903a144fa46e55d
Aleksandr Kuznetsov <caiman@codecrafte.rs>
2018-09-08 16:26:43 +0300

    [WB-14] Create database shell

--- src/tasks.py
+++ src/tasks.py
@@ -2,9 +2,11 @@ from invoke import Collection
 
 from api.tasks import ns as api_tasks
 from commons.tasks import shell
+from db.tasks import ns as db_tasks
 
 
 ns = Collection()
 ns.add_task(shell)
 
 ns.add_collection(api_tasks)
+ns.add_collection(db_tasks)
