aa90130319d094b29649b167067776c4d0d28faf
vodono <vdn2004@gmail.com>
2018-09-08 23:33:29 +0300

    [WB-11]Add initial logging

---
+++ tests/test_logging/test_logging.py
@@ -0,0 +1,8 @@
+import pytest
+from log.logger_to_file import LoggerToFile
+
+
+# def test_logging():
+#     logger = LoggerToFile(__name__)
+#     logger.warning("Info")
+#     assert logger.warning("Info") == 'Info'
