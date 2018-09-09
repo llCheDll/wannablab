474c59543fd3b6c731b7c167e1800e299f283ad5
vodono <vdn2004@gmail.com>
2018-09-09 15:24:19 +0300

    [WB-11]Correct logging

--- src/api/handlers.py
+++ src/api/handlers.py
@@ -2,10 +2,12 @@ import ujson
 import falcon
 
 from .constants import Status
-
+from log.logger import Logger
 
 class Ping:
     def on_get(self, req, resp):
         resp.set_header('Content-Type', 'application/json')
         resp.status = falcon.HTTP_200
         resp.body = ujson.dumps({'status': Status.OK})
+        logger = Logger(f"{__name__}.ping")
+        logger.info(resp.body)
--- tests/test_logging/test_logging.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from log.logger_to_file import LoggerToFile
-
-
-# def test_logging():
-#     logger = LoggerToFile(__name__)
-#     logger.warning("Info")
-#     assert logger.warning("Info") == 'Info'
