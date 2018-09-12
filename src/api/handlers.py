cc5504f2e0f5e126dd3c6047e5c502ef35462c10
emerell <valeriya.mihaleva.1999@gmail.com>
2018-09-12 23:54:39 +0300

    [WB-19] add improvements for handlers

--- src/api/handlers.py
+++ src/api/handlers.py
@@ -1,6 +1,7 @@
 import ujson
 import falcon
 
+from sqlalchemy.orm.exc import NoResultFound
 from .constants import Status
 from .helpers import row2dict
 from db import models
@@ -42,13 +43,76 @@ class Country(Items):
     model = models.Country
 
 
-class City(Items):
-    model = models.City
+class Region:
+    def on_get(self, request, response, country_id):
+
+        try:
+            session = request.context['session']
+            regions = session.query(models.Region).filter(
+                models.Region.country_id == country_id).all()
+
+            response.set_header('Content-Type', 'application/json')
+            response.status = falcon.HTTP_200
+            response.body = ujson.dumps(
+                {
+                    'status': Status.OK,
+                    'data': [row2dict(region) for region in regions]
+                }
+            )
+        except NoResultFound:
+            response.status = falcon.HTTP_404
+            response.body = ujson.dumps(
+                {
+                    'status': Status.NotFound,
+                }
+            )
 
 
-class Region(Items):
-    model = models.Region
+class City:
+    def on_get(self, request, response, country_id):
+
+        try:
+            session = request.context['session']
+            cities = session.query(models.City).join(models.Region).filter(
+                models.Region.country_id == country_id)
+
+            response.set_header('Content-Type', 'application/json')
+            response.status = falcon.HTTP_200
+            response.body = ujson.dumps(
+                {
+                    'status': Status.OK,
+                    'data': [row2dict(city) for city in cities]
+                }
+            )
+        except NoResultFound:
+            response.status = falcon.HTTP_404
+            response.body = ujson.dumps(
+                {
+                    'status': Status.NotFound,
+                }
+            )
 
 
-class District(Items):
-    model = models.District
+class District:
+    def on_get(self, request, response, city_id):
+
+        try:
+            session = request.context['session']
+            districts = session.query(models.District).filter(
+                models.District.city_id == city_id).all()
+
+            response.set_header('Content-Type', 'application/json')
+            response.status = falcon.HTTP_200
+            response.body = ujson.dumps(
+                {
+                    'status': Status.OK,
+                    'data': [row2dict(district) for district in districts]
+                }
+            )
+        except NoResultFound:
+            response.status = falcon.HTTP_404
+            response.body = ujson.dumps(
+                {
+                    'status': Status.NotFound,
+                }
+            )
--- tests/test_api/test_location.py
+++ tests/test_api/test_location.py
@@ -1,7 +1,80 @@
 import falcon
 
-from db.models import Country, Region, City, District
+from db.models import Country, Region, City
 from api.constants import Status
+
+
+def test_city(session, client):
+    cities = []
+
+    country = Country(
+        id=2,
+        title='Ukraine',
+    )
+
+    region = Region(
+        id=2,
+        title='Ivano-Frankivsk region',
+        country_id=2
+    )
+    for title in ['Ivano-Frankivsk', 'Kharkiv']:
+        cities.append(
+            City(
+                title=title,
+                region_id=2
+            )
+        )
+
+    session.add(country)
+    session.add(region)
+    session.add_all(cities)
+    session.commit()
+
+    resp = client.get('/api/v1/2/city/', as_response=True)
+
+    assert resp.status == falcon.HTTP_OK
+    assert resp.json['status'] == Status.OK
+
+    for obj in resp.json['data']:
+        assert obj['title'] in ['Ivano-Frankivsk', 'Kharkiv']
+
+    session.query(City).filter().delete()
+    session.query(Region).filter().delete()
+    session.query(Country).filter().delete()
+    session.commit()
+
+
+def test_region(session, client):
+    regions = []
+
+    country = Country(
+        id=2,
+        title='Ukraine',
+    )
+
+    for title in ['Ivano-Frankivsk', 'Kharkiv']:
+        regions.append(
+            Region(
+                title=title,
+                country_id=2
+            )
+        )
+
+    session.add(country)
+    session.add_all(regions)
+    session.commit()
+
+    resp = client.get('/api/v1/2/region/', as_response=True)
+
+    assert resp.status == falcon.HTTP_OK
+    assert resp.json['status'] == Status.OK
+
+    for obj in resp.json['data']:
+        assert obj['title'] in ['Ivano-Frankivsk', 'Kharkiv']
+
+    session.query(Region).filter().delete()
+    session.query(Country).filter().delete()
+    session.commit()
 
 
 def test_countries(session, client):
@@ -25,72 +98,3 @@ def test_countries(session, client):
         assert obj['title'] in ['Ukraine', 'Poland']
 
     session.query(Country).filter().delete()
-
-
-def test_region(session, client):
-    regions = []
-
-    for title in ['Ivano-Frankivsk', 'Kharkiv']:
-        regions.append(
-            Region(
-                title=title
-            )
-        )
-    session.add_all(regions)
-    session.commit()
-
-    resp = client.get('/api/v1/region/', as_response=True)
-
-    assert resp.status == falcon.HTTP_OK
-    assert resp.json['status'] == Status.OK
-
-    for obj in resp.json['data']:
-        assert obj['title'] in ['Ivano-Frankivsk', 'Kharkiv']
-
-    session.query(Region).filter().delete()
-
-
-def test_city(session, client):
-    cities = []
-
-    for title in ['Ivano-Frankivsk', 'Kharkiv']:
-        cities.append(
-            City(
-                title=title
-            )
-        )
-    session.add_all(cities)
-    session.commit()
-
-    resp = client.get('/api/v1/city/', as_response=True)
-
-    assert resp.status == falcon.HTTP_OK
-    assert resp.json['status'] == Status.OK
-
-    for obj in resp.json['data']:
-        assert obj['title'] in ['Ivano-Frankivsk', 'Kharkiv']
-
-    session.query(City).filter().delete()
-
-
-def test_district(session, client):
-    districts = []
-
-    for title in ['Dniprovskyi', 'Shevchenkivskyi']:
-        districts.append(
-            District(
-                title=title
-            )
-        )
-    session.add_all(districts)
-    session.commit()
-
-    resp = client.get('/api/v1/district/', as_response=True)
-
-    assert resp.status == falcon.HTTP_OK
-    assert resp.json['status'] == Status.OK
-
-    for obj in resp.json['data']:
-        assert obj['title'] in ['Dniprovskyi', 'Shevchenkivskyi']
-
-    session.query(District).filter().delete()
